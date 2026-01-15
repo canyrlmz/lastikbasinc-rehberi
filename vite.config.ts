8ea6a81e562e892fe9f313c11403f0df3f7f2bd9
Sevket Can Yorulmaz <can7881@hotmail.com>
2026-01-16 00:19:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -6,7 +5,6 @@ import path from 'path';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // DİKKAT: Burası senin repo adınla BİREBİR AYNI olmalı
       base: '/lastikbasinc-rehberi/', 
       server: {
         port: 3000,
@@ -14,8 +12,9 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        // || '' ekleyerek undefined hatasını önlüyoruz
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
       },
       resolve: {
         alias: {
